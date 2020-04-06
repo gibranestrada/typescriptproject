70e07b92f20a8ae891aedd5f4a3ecc359fe8f040
Gibran Estrada <gibran9@gmail.com>
2020-04-06 09:14:25 -0400

    new pc

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -10,7 +10,7 @@ import {
 import styles from './Counter.module.css';
 
 export const Counter: React.FC = () => {
-  const count = useSelector(selectCount);
+  const count = useSelector(selectCount);//
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
   console.log('what 1')
